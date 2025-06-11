9e0b84819a54e4096bea7c7fb23277ec4b458820
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-06-11 15:06:55 +0500

    Created (buggy) calculator functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let number1 = 0;
-let number2 = 0;
-let operator = '';
+let number1 = null;
+let number2 = null;
+let operator = null;
 
 function add(a,b){
     return a + b;
@@ -18,84 +18,188 @@ function divide(a,b){
     return a/b;
 }
 
-function operate(operator,number1,number2){
+function operate(operator, number1, number2){
+    number1 = parseFloat(number1)
+    number2 = parseFloat(number2)
     switch(operator){
         case "plus":
-            add(number1,number2);
-            break;
+            return String(add(number1,number2));
         case "minus":
-            subtract(number1,number2);
-            break;
+            return String(subtract(number1,number2));
         case "times":
-            multiply(number1,number2);
-            break;
+            return String(multiply(number1,number2));
         case "dividedBy":
-            divide(number1,number2);
-            break;
+            return String(divide(number1,number2));
         default:
             console.log("Error in operate().");
     }
 }
 
 function createEventListeners(){
-    const allButtons = document.querySelectorAll(
-        "#zero","#one","#two","#three","#four",
-        "#five", "#six", "#seven", "#eight", "#nine", 
-        "#clear", "#del", "#point", "#equals",
-        "#divideBy", "#times", "#minus", "#plus"
-    )
-
+    //Initialize all buttons
     const zeroButton = document.getElementById("zero");
-    zeroButton.addEventListener("click", takeInput(0));
+    zeroButton.addEventListener("click", function() {
+        takeInput("0");});
 
     const oneButton = document.getElementById("one");
-    oneButton.addEventListener("click", takeInput(1));
+    oneButton.addEventListener("click", function() {
+        takeInput("1");});
 
     const twoButton = document.getElementById("two");
-    twoButton.addEventListener("click", takeInput(2));
+    twoButton.addEventListener("click", function() {
+        takeInput("2");});
 
     const threeButton = document.getElementById("three");
-    threeButton.addEventListener("click", takeInput(3));
+    threeButton.addEventListener("click", function() {
+        takeInput("3");});
 
     const fourButton = document.getElementById("four");
-    fourButton.addEventListener("click", takeInput(4));
+    fourButton.addEventListener("click", function() {
+        takeInput("4");});
 
     const fiveButton = document.getElementById("five");
-    fiveButton.addEventListener("click", takeInput(5));
+    fiveButton.addEventListener("click", function() {
+        takeInput("5");});
 
     const sixButton = document.getElementById("six");
-    sixButton.addEventListener("click", takeInput(6));
+    sixButton.addEventListener("click", function() {
+        takeInput("6");});
 
     const sevenButton = document.getElementById("seven");
-    sevenButton.addEventListener("click", takeInput(7));
+    sevenButton.addEventListener("click", function() {
+        takeInput("7");});
 
     const eightButton = document.getElementById("eight");
-    eightButton.addEventListener("click", takeInput(8));
+    eightButton.addEventListener("click", function() {
+        takeInput("8");});
 
     const nineButton = document.getElementById("nine");
-    nineButton.addEventListener("click", takeInput(9));
-
-    const clearButton = document.getElementById("clear");
-    clearButton.addEventListener("click", takeInput("clear"));
+    nineButton.addEventListener("click", function() {
+        takeInput("9");});
     
     const delButton = document.getElementById("del");
-    delButton.addEventListener("click", takeInput("del"));
+    delButton.addEventListener("click", function() {
+        takeInput("del");});
 
     const pointButton = document.getElementById("point");
-    pointButton.addEventListener("click", takeInput("point"));
+    pointButton.addEventListener("click", function() {
+        takeInput("point");});
 
     const equalsButton = document.getElementById("equals");
-    equalsButton.addEventListener("click", takeInput("equals"));
+    equalsButton.addEventListener("click", function() {
+        takeInput("equals");});
 
-    const divideByButton = document.getElementById("divideBy");
-    divideByButton.addEventListener("click", takeInput("divideBy"));
+    const dividedByButton = document.getElementById("dividedBy");
+    dividedByButton.addEventListener("click", function() {
+        takeInput("dividedBy");});
 
     const timesButton = document.getElementById("times");
-    timesButton.addEventListener("click", takeInput("times"));
+    timesButton.addEventListener("click", function() {
+        takeInput("times");});
 
     const minusButton = document.getElementById("minus");
-    minusButton.addEventListener("click", takeInput("minus"));
+    minusButton.addEventListener("click", function() {
+        takeInput("minus");});
 
     const plusButton = document.getElementById("plus");
-    plusButton.addEventListener("click", takeInput("plus"));
+    plusButton.addEventListener("click", function() {
+        takeInput("plus");});
+
+    const allClearButton = document.getElementById("allClear")
+    allClearButton.addEventListener("click", function() {
+        takeInput("allClear");});
+
+    //Initialize the screen
+    const display = document.getElementById("display");
 }
+
+function takeInput(input){
+    const numbersAsStrings = ["0","1","2","3","4","5","6","7","8","9"];
+    const operatorsAsStrings = ["divideBy", "times", "minus", "plus"];
+    const equals = ["equals"];
+    const clear = ["allClear"];
+    if (
+        numbersAsStrings.includes(input) && 
+        number1 === null)
+    {
+        number1 = input;
+        updateDisplay(number1)
+    } else if (
+        numbersAsStrings.includes(input) && 
+        number1 != null &&
+        operator === null)
+    {
+        number1 += input;
+        updateDisplay(number1)
+    } else if (
+        operatorsAsStrings.includes(input) && 
+        number1 != null &&
+        number2 === null)
+    {
+        operator = input;
+        updateDisplay();
+    } else if (
+        operatorsAsStrings.includes(input) &&
+        number1 === null)
+    {
+        //do nothing;
+    } else if (
+        numbersAsStrings.includes(input) &&
+        operator != null &&
+        number1 != null)
+    {
+        number2 = input;
+        updateDisplay(number2)
+    } else if (
+        numbersAsStrings.includes(input) &&
+        operator != null &&
+        number1 != null &&
+        number2 != null)
+    {
+        number2 += null;
+        updateDisplay(number2)
+    } else if (
+        operatorsAsStrings.includes(input) &&
+        operator != null &&
+        number1 != null &&
+        number2 != null)
+    {
+        number1 = operate(operator,number1,number2);
+        number2 = null;
+        operator = null;
+        updateDisplay(number1);
+    } else if (
+        equals.includes(input) &&
+        number1 &&
+        number2 &&
+        operator)
+    {
+        number1 = operate(operator,number1,number2);
+        number2 = null;
+        operator = null;
+        updateDisplay(number1);
+    } else if (
+        clear.includes("allClear"))
+    {
+        allClear();
+    }
+
+    function updateDisplay(input){
+        if (input){
+            display.textContent = input;
+        } else {
+            display.innerHTML = "";
+        }
+    }
+
+    function allClear(){
+        number1 = null;
+        number2 = null;
+        operator = null;
+        updateDisplay();
+    }
+}
+
+
+
+createEventListeners();
