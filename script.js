5b3f4560cac0ec8ef50fcab405f3ff077cd5239d
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-06-16 13:19:16 +0500

    Remove rounding

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ function operate(operator, number1, number2){
     number2 = parseFloat(number2);
     switch(operator){
         case "plus":
-            return add(number1,number2).toFixed(6);
+            return add(number1,number2);
         case "minus":
-            return subtract(number1,number2).toFixed(6);
+            return subtract(number1,number2);
         case "times":
-            return multiply(number1,number2).toFixed(6);
+            return multiply(number1,number2);
         case "dividedBy":
-            return divide(number1,number2).toFixed(6);
+            return divide(number1,number2);
         default:
             console.log("Error in operate().");
     }
